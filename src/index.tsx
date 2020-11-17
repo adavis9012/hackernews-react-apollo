83ac1353fb4cc7a7b39abc132ca792350355017c
David Velandia <adavis9012@gmail.com>
2020-11-16 22:31:19 -0500

    call and integrate apollo context with client to send authorization token on header

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './components/App';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import {AUTH_TOKEN} from "./constants";
 import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from '@apollo/client';
 import {BrowserRouter} from "react-router-dom";
+import {setContext} from "@apollo/client/link/context";
+
+import './styles/index.css';
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, {headers}) => {
+    const token = localStorage.getItem(AUTH_TOKEN);
+
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+});
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
 
